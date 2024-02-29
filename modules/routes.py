9867a5b8e38bb73e455d55a430faa6729bcaa535
stone <vannakseng1996@gmail.com>
2024-02-29 10:13:36 +0700

    init

--- modules/routes.py
+++ modules/routes.py
@@ -2,10 +2,4 @@ from fastapi import FastAPI
 
 
 def set_route(app: FastAPI):
-    from modules.user.controller import router as user_router
-
-    app.include_router(user_router)
-
-    from modules.teacher.controller import router as teacher_router
-
-    app.include_router(teacher_router)
+    pass
