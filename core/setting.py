dc853f8de4940c7961b17d1fcd6b6ddb02ce8b11
jinghong <Jinghongbob@gmail.com>
2024-02-29 11:34:45 +0700

    new data base

--- core/setting.py
+++ core/setting.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     app_name: str = os.getenv("APP_NAME", "My Api")
     database_url: str = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:P%40ssw0rd@localhost/school-system-management",
+        "postgresql://postgres:password@localhost/Company"
     )
     secret_key: str = os.getenv("SECRET_KEY", "9cfe4731588484c858a3900c71f57833")
     jwt_issuer: str = os.getenv("JWT_ISSUER", "3b532505bb1afbbe537527343f78e048")
