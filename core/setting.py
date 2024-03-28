792bc516b5d5f4e2d4056d22e6f30438433d433c
Cheanlong512 <cheanlongwu@gmail.com>
2024-03-28 10:23:22 +0700

    Added: DB render

--- core/setting.py
+++ core/setting.py
@@ -8,8 +8,8 @@ class Settings(BaseSettings):
     app_name: str = os.getenv("APP_NAME", "My Api")
     database_url: str = os.getenv(
         "DATABASE_URL",
-        # "postgres://master_long_db:nzYjkYBawfH4mLLKZwtvBoEUXDFH1lfT@dpg-cnp7otn109ks73esml3g-a.singapore-postgres.render.com/supplychain_api_db",
-        "postgresql://postgres:password@localhost/postgres",
+        "postgresql://sca_db_user:hypuQ56icNudfDDYJxeWQSG2I5GquyAF@dpg-co2djosf7o1s73ch744g-a.singapore-postgres.render.com/sca_db",
+        #"postgresql://postgres:password@localhost/postgres",
     )
     secret_key: str = os.getenv("SECRET_KEY", "9cfe4731588484c858a3900c71f57833")
     jwt_issuer: str = os.getenv("JWT_ISSUER", "3b532505bb1afbbe537527343f78e048")
