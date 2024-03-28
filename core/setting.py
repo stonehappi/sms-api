c3ea6055fabb404a9653505a539fa30cb8e4adce
jinghong <Jinghongbob@gmail.com>
2024-03-28 10:24:42 +0700

    final

--- core/setting.py
+++ core/setting.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     app_name: str = os.getenv("APP_NAME", "My Api")
     database_url: str = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:password@localhost/Company"
+        "postgresql://jinghong_user:XgX3EiI6x6xvsoQuGGeqJuHCMUeWDUNm@dpg-co2dk38l6cac73bm11eg-a.singapore-postgres.render.com/jinghong"
     )
     secret_key: str = os.getenv("SECRET_KEY", "9cfe4731588484c858a3900c71f57833")
     jwt_issuer: str = os.getenv("JWT_ISSUER", "3b532505bb1afbbe537527343f78e048")
